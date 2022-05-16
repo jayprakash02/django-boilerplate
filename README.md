3045621d834f28eb99748d64cd5cc92bea681977
jayprakash02 <unijay12@gmail.com>
2022-05-16 08:51:13 +0530

    postgres added and README.md updated

--- README.md
+++ README.md
@@ -8,3 +8,27 @@
 - Docker
 - React
 - Postgres
+
+### Postgres Setup on docker for development
+```
+docker run -d \
+	--name django-boilerplate-db \
+	-e POSTGRES_USER=django \
+	-e POSTGRES_DB=django_boilerplate \
+	-e POSTGRES_PASSWORD=postgres \
+	-e PGDATA=/var/lib/postgresql/data/pgdata \
+	-p 5432:5432 \
+	-v /home/postgresql/django:/var/lib/postgresql/data \
+	postgres:13.5
+```
+
+### Environment file
+##### Create a ```.env``` file in root directory with bellow mentioned data:
+
+| Variable    | Value |
+|-------------|-------|
+| DB_USER     | -     |
+| DB_NAME     | -     |
+| DB_PASSWORD | -     |
+| DB_HOST     | -     |
+| DB_PORT     | -     | 
--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config['DB_NAME'],
+        'USER': config['DB_USER'],
+        'PASSWORD': config['DB_PASSWORD'],
+        'HOST': config['DB_HOST'],
+        'PORT': config['DB_PORT'],
     }
 }
 
